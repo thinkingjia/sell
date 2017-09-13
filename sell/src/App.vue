5464dc8fbf3d1575274f33d1e8f2dfa34312fd8f
thinkingjia <309533855@qq.com>
2017-09-13 11:38:33 +0800

    组件拆分(2)
    修改的样式部分

--- sell/src/App.vue
+++ sell/src/App.vue
@@ -1,12 +1,35 @@
 <template>
   <div id="app">
-    
+    <v-header></v-header>
+    <div class="tab">
+      <div class="tab-item">商品</div>
+      <div class="tab-item">评论</div>
+      <div class="tab-item">商家</div>
+    </div>
+    <div class="content">
+      i am content
+    </div>
   </div>
 </template>
 
 <script>
+  import header from './components/header/header.vue';
+
+  export default {
+    components: {
+      'v-header': header
+    }
+  };
 </script>
 
-<style>
-
+<style lang="stylus" rel="stylesheet/stylus">
+#app
+  .tab
+    display: flex
+    width: 100%
+    height: 40px
+    line-height: 40px
+    .tab-item
+      flex: 1
+      text-algin: center
 </style>
